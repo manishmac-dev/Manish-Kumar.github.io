6560f0672eea93098270f5bbd9b3b8abd9d16e7c
manishmac-dev <mkrg102@gmail.com>
2021-06-22 09:59:21 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -12,9 +12,11 @@ if(empty($name) || empty($email) || empty($phone) || empty($message))
 }
 else
 {
-	mail("mkrg102@gmail.com", "Manish Kumar", $message);
+	mail("mkrg102@gmail.com","Manish",
+	$_POST["message"]. "From: mackumar02@gmail.com");
 	echo"<script type='text/javascript'>alert('your message sent seccessfully');
 	window.history.log(-1);
 	</script>";
 }
 ?>
+<!-- Failed to connect to mailserver at "localhost" port 25, verify your "SMTP" and "smtp_port" setting in php.ini or use ini_set() -->
